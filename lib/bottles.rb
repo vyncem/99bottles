62e46966ed2a8b0f49e91dcc2dea0ec622394683
Victor Muia <victor.muia@storyful.com>
2018-05-09 16:38:51 +0100

    Refactor for rubocop

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,15 +1,18 @@
+# frozen_string_literal: true
+
+# 99 bottle of beer on the wall song
 class Bottles
   def verse(bottles)
-    return <<-VERSE
-#{no_bottles(bottles, true)} #{pluralize_bottles(bottles)} of beer on the wall, #{no_bottles(bottles)} #{pluralize_bottles(bottles)} of beer.
-#{line_two(bottles)}
-VERSE
+    <<~VERSE
+      #{no_bottles(bottles, true)} #{pluralize_bottles(bottles)} of beer on the wall, #{no_bottles(bottles)} #{pluralize_bottles(bottles)} of beer.
+      #{line_two(bottles)}
+    VERSE
   end
 
   def verses(start, finish)
     args_range = (finish..start).to_a.reverse
 
-    args_range.map{ |arg| verse(arg) }.join("\n")
+    args_range.map { |arg| verse(arg) }.join("\n")
   end
 
   def song
@@ -17,16 +20,23 @@ VERSE
   end
 
   private
-  def line_two(bottles)
 
-    if bottles > 0
-      "Take #{pluralize_one(bottles)} down and pass it around, #{remaining_bottles(bottles)} #{pluralize_bottles(remaining_bottles(bottles))} of beer on the wall."
+  def line_two(bottles)
+    if bottles.positive?
+      "Take #{pluralize_one(bottles)} down and pass it around, "\
+      "#{remaining_bottles(bottles)} "\
+      "#{pluralize_bottles(remaining_bottles(bottles))} of beer on the wall."
     else
-      "Go to the store and buy some more, 99 bottles of beer on the wall."
+      'Go to the store and buy some more, 99 bottles of beer on the wall.'
     end
   end
-  def no_bottles(bottles, sentense_case=false)
-    bottles > 0 ? bottles  : (sentense_case ? 'No more' : 'no more')
+
+  def no_bottles(bottles, sentense_case = false)
+    bottles.positive? ? bottles : no_more(sentense_case)
+  end
+
+  def no_more(sentense_case)
+    sentense_case ? 'No more' : 'no more'
   end
 
   def remaining_bottles(bottles)
@@ -34,11 +44,10 @@ VERSE
   end
 
   def pluralize_bottles(bottles)
-    (bottles.is_a?(String) || bottles != 1) ? 'bottles' : 'bottle'
+    bottles.is_a?(String) || bottles != 1 ? 'bottles' : 'bottle'
   end
 
   def pluralize_one(bottles)
     bottles > 1 ? 'one' : 'it'
   end
 end
-
